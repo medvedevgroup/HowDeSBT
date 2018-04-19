0e928c51ded17c124b3b19114e4d9754b6108201
Bob Harris <rsharris@bx.psu.edu>
2018-04-19 16:23:57 -0400

    version bump to 0.2.3

--- cmd_version.h
+++ cmd_version.h
@@ -13,8 +13,8 @@ class VersionCommand: public Command
 public:
 	static const unsigned int  major    = 0;
 	static const unsigned int  minor    = 2;
-	static const unsigned int  subMinor = 2;
-	static const std::uint32_t date     = 0x20180410;
+	static const unsigned int  subMinor = 3;
+	static const std::uint32_t date     = 0x20180419;
 
 public:
 	VersionCommand(const std::string& name): Command(name) {}
