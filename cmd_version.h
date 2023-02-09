a8d7a1fc3879ae2f933deaba470a8b9ef8ce7a30
Bob Harris <rsharris@bx.psu.edu>
2023-02-09 11:30:04 -0500

    bump version to 2.0.13 in prep for tagging it

--- cmd_version.h
+++ cmd_version.h
@@ -13,8 +13,8 @@ class VersionCommand: public Command
 public:
 	static const unsigned int  major    = 2;
 	static const unsigned int  minor    = 0;
-	static const unsigned int  subMinor = 12;
-	static const std::uint32_t date     = 0x20230205;
+	static const unsigned int  subMinor = 13;
+	static const std::uint32_t date     = 0x20230209;
 
 public:
 	VersionCommand(const std::string& name): Command(name) {}
