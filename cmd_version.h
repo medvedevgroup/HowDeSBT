f1487e837fc2226acb722ad0cab1f73324d7a52b
Bob Harris <rsharris@bx.psu.edu>
2024-04-19 15:16:05 -0400

    .

--- cmd_version.h
+++ cmd_version.h
@@ -13,8 +13,8 @@ class VersionCommand: public Command
 public:
 	static const unsigned int  major    = 2;
 	static const unsigned int  minor    = 0;
-	static const unsigned int  subMinor = 14;
-	static const std::uint32_t date     = 0x20230209;
+	static const unsigned int  subMinor = 15;
+	static const std::uint32_t date     = 0x20240419;
 
 public:
 	VersionCommand(const std::string& name): Command(name) {}
